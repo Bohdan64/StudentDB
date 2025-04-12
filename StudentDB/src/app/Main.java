790d666feaa32884be74b0a9bce326b22a3e74f7
Bohdan64 <271330@vutbr.cz>
2025-04-13 01:50:12 +0200

    CyberStudent

--- StudentDB/src/app/Main.java
+++ StudentDB/src/app/Main.java
@@ -1,25 +1,45 @@
 package app;
 
+import model.CyberStudent;
 import model.TelecomStudent;
 
 public class Main {
     public static void main(String[] args) {
         
-        TelecomStudent student1 = new TelecomStudent("Ivan", "Novak", 2000);
+        TelecomStudent telecomStudent = new TelecomStudent("Ivan", "Novak", 2000);
+        
+        System.out.println("Telecom Student:");
+        System.out.println("ID: " + telecomStudent.getId());
+        System.out.println("Jméno: " + telecomStudent.getJmeno());
+        System.out.println("Příjmení: " + telecomStudent.getPrijmeni());
+        System.out.println("Rok narození: " + telecomStudent.getYear());
+        System.out.println("Průměr známek: " + telecomStudent.getAverageGrade());
 
-        System.out.println("ID: " + student1.getId());
-        System.out.println("Jméno: " + student1.getJmeno());
-        System.out.println("Příjmení: " + student1.getPrijmeni());
-        System.out.println("Rok narození: " + student1.getYear());
-        System.out.println("Průměr známek: " + student1.getAverageGrade());
+        telecomStudent.addGrade(1);
+        telecomStudent.addGrade(2);
+        telecomStudent.addGrade(3);
 
-        student1.addGrade(1);
-        student1.addGrade(2);
-        student1.addGrade(3);
-
-        System.out.println("Průměr známek po přidání 1,2,3: " + student1.getAverageGrade());
+        System.out.println("Průměr známek po přidání 1,2,3: " + telecomStudent.getAverageGrade());
 
         System.out.println("Morse kód:");
-        student1.showSkill();
+        telecomStudent.showSkill();
+
+        System.out.println();
+        
+        CyberStudent cyberStudent = new CyberStudent("Ivan", "Novak", 2000);
+
+        System.out.println("Cyber Student:");
+        System.out.println("ID: " + cyberStudent.getId());
+        System.out.println("Jméno: " + cyberStudent.getJmeno());
+        System.out.println("Příjmení: " + cyberStudent.getPrijmeni());
+        System.out.println("Rok narození: " + cyberStudent.getYear());
+
+        cyberStudent.addGrade(5);
+        cyberStudent.addGrade(4);
+        cyberStudent.addGrade(3);
+
+        System.out.println("Průměr známek po přidání 5,4,3: " + cyberStudent.getAverageGrade());
+
+        cyberStudent.showSkill();
     }
 }
--- StudentDB/src/model/CyberStudent.java
+++ StudentDB/src/model/CyberStudent.java
@@ -1,5 +1,42 @@
-	package model;
+package model;
 
-public class CyberStudent {
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.List;
+
+public class CyberStudent extends Student {
+
+	public CyberStudent(int id, String jmeno, String prijmeni, int year, List<Integer> grades) {
+		super(id, jmeno, prijmeni, year, grades);
+	}
+
+	public CyberStudent(String jmeno, String prijmeni, int year) {
+		super(jmeno, prijmeni, year);
+	}
+	
+	@Override
+	public void showSkill() {
+		String fullName = (jmeno + " " + prijmeni).toUpperCase();
+		
+		try {
+			MessageDigest mDigest = MessageDigest.getInstance("SHA-256");
+			
+			byte[] hashBytes = mDigest.digest(fullName.getBytes());
+			
+			StringBuilder hexString = new StringBuilder();
+			for (byte b : hashBytes) {
+				String hex = Integer.toHexString(0xff & b);
+				if (hex.length() == 1) hexString.append(0);
+				hexString.append(hex);
+			}
+			
+	        String hash = hexString.toString();
+	        System.out.println("SHA-256 Haš celé jméno: " + hash);
+	        
+		} catch (NoSuchAlgorithmException e) {
+			throw new RuntimeException("SHA-256 Error", e);
+		}
+	}
+	
 
 }
